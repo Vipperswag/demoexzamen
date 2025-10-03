fa8b5e0bbee62198f09b3a3774ec1bb2018a6dc6
kabinet303 <kabinet303@localhost.localdomain>
2025-10-03 09:39:05 +0300

    Структура

---
+++ narysheniam_net/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Нарушениям нет</title>
+    <link rel="icon" href="images/1.jpeg">
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+    <header>
+    <img src="images/1.jpeg" alt="Логотип">
+    <h1>Нарушениям нет</h1>
+    </header>
+<nav>
+    <a href="/mou_ne_sam">Главная</a>
+    <a href="/mou_ne_sam/admin">Админ</a>
+</nav>
+
+    <main>
+        <footer>
+        <h3>2025</h3>
+        </footer>
+    </main>
+<script src="script/script.js"></script>
+</body>
+</html>
